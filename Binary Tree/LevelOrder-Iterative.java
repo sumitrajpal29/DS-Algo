cc830c73e77450ac94af92dd5eb17a14b4383170
Sumit Rajpal <70939894+sumitrajpal29@users.noreply.github.com>
2022-10-15 17:58:47 +0530

    Update LevelOrder-Iterative.java

--- Binary Tree/LevelOrder-Iterative.java
+++ Binary Tree/LevelOrder-Iterative.java
@@ -1,1 +1,26 @@
-
+class Solution
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        // Your code here
+        ArrayList<Integer> list=new ArrayList<>();
+      
+        Queue<Node> q=new LinkedList<>();
+            q.offer(node);
+      
+            Node temp=null;
+        while(!q.isEmpty()){
+            temp=q.poll();
+            
+            list.add(temp.data);
+            
+            if(temp.left!=null)
+                q.offer(temp.left);
+            if(temp.right!=null)
+                q.offer(temp.right);
+        }
+        
+        return list;
+    }
+}
